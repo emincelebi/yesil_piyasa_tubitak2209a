e3ff0ec3b1ec9bb9f28a55723b7bf4adae254235
Mustafa Kıraç <kiracmustafa82@gmail.com>
2024-10-10 13:42:07 +0300

    test degisikligi

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:yesilpiyasa/main.dart';
+import 'package:yesil_piyasa/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
