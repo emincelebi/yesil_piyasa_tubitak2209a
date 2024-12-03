0809c5998689776a3d240a957538663a57b1218a
Emin Celebi <emincelebi.sw@gmail.com>
2024-12-03 16:49:13 +0300

    Detaylar sayfasındaki hata giderildi

--- lib/core/widgets/product_detail_page.dart
+++ lib/core/widgets/product_detail_page.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:yesil_piyasa/model/product.dart';
 import 'package:yesil_piyasa/viewmodel/user_model.dart';
 
@@ -76,6 +78,65 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
         return 'assets/images/tahil.jpg';
       default:
         return 'assets/images/diger.jpg';
+    }
+  }
+
+  void _showContactDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text(
+            'İletişim Bilgileri',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                GestureDetector(
+                  onTap: () => _launchPhoneNumber(
+                      'tel:+901234567890'), // Telefon numarasına tıklanırsa arama yapar
+                  child: const Text(
+                    'Telefon: +90 123 456 7890',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'E-posta: example@example.com',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                'Kapat',
+                style: TextStyle(fontSize: 18),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _launchPhoneNumber(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Telefon numarası açılamadı: $url';
     }
   }
 
@@ -156,53 +217,97 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                         ),
                       ),
                       const SizedBox(height: 20),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  "Stok",
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.white70,
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    "Stok",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.white70,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  "${widget.product.stock}",
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
+                                  Text(
+                                    "${widget.product.stock}",
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  "Kategori",
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.white70,
+                            Expanded(
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    "Kategori",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.white70,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  getCategoryNames(widget.product.category),
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
+                                  Text(
+                                    getCategoryNames(widget.product.category),
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  const Text(
+                                    'Birim Fiyatı',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.white70,
+                                    ),
+                                  ),
+                                  Text(
+                                    '${widget.product.price} ₺ / ${widget.product.unit}',
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  const Text(
+                                    'Satış Tarihi',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.white70,
+                                    ),
+                                  ),
+                                  Text(
+                                    '${widget.product.createdAt}',
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                       const Spacer(),
                       Row(
@@ -231,15 +336,20 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                               final likes = productData['likes'] ?? 0;
                               return Text(
                                 "Beğeniler: $likes",
-                                style: const TextStyle(color: Colors.white70),
+                                style: const TextStyle(
+                                    color: Colors.white70, fontSize: 20),
                               );
                             },
                           ),
                           const Spacer(),
                           IconButton(
-                            icon: const Icon(Icons.share, color: Colors.white),
+                            icon: const FaIcon(
+                              FontAwesomeIcons.phoneAlt, // Telefon ikonu
+                              color: Colors.white,
+                              size: 30,
+                            ),
                             onPressed: () {
-                              // Paylaşım işlemleri
+                              _showContactDialog(context); // Dialog aç
                             },
                           ),
                         ],
@@ -252,15 +362,31 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          isFavorite ? Icons.favorite : Icons.favorite_border,
-          color: isFavorite ? Colors.red : null,
+      floatingActionButton: Container(
+        width: 60.0, // Buton genişliği
+        height: 60.0, // Buton yüksekliği
+        decoration: BoxDecoration(
+          color: Colors.white, // Buton arka planı beyaz
+          shape: BoxShape.circle, // Yuvarlak şekil
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 4.0,
+              offset: const Offset(0, 2),
+            ),
+          ],
         ),
-        onPressed: () {
-          toggleFavorite(widget.product.productID);
-        },
+        child: IconButton(
+          icon: Icon(
+            isFavorite ? Icons.favorite : Icons.favorite_border,
+            color: isFavorite ? Colors.red : Colors.black, // İkon rengi
+          ),
+          onPressed: () {
+            toggleFavorite(widget.product.productID);
+          },
+        ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndTop,
     );
   }
 }
